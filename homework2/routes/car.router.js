7827756a1722b524101dc1010103199c536c8a1f
oljakostiv <oljakostivv@gmail.com>
2021-08-27 03:08:38 +0300

    some change.

--- homework2/routes/car.router.js
+++ homework2/routes/car.router.js
@@ -5,8 +5,9 @@ const { carMiddle } = require('../middlewares');
 
 router.get('/', carsController.getAllCars);
 router.post('/', carMiddle.checkUniqueModel, carsController.setCar);
-router.put('/:car_id', carMiddle.isCarPresent, carMiddle.checkUniqueModel, carsController.updateCar);
+
 router.delete('/:car_id', carMiddle.isCarPresent, carsController.deleteCar);
 router.get('/:car_id', carMiddle.isCarPresent, carsController.getSingleCar);
+router.put('/:car_id', carMiddle.isCarPresent, carMiddle.checkUniqueModel, carsController.updateCar);
 
 module.exports = router;
--- homework2/routes/users.router.js
+++ homework2/routes/users.router.js
@@ -5,8 +5,9 @@ const { userMiddle } = require('../middlewares');
 
 router.get('/', usersController.getAllUsers);
 router.post('/', userMiddle.checkUniqueName, usersController.setUser);
-router.put('/:user_id', userMiddle.isUserPresent, userMiddle.checkUniqueName, usersController.updateUser);
+
 router.delete('/:user_id', userMiddle.isUserPresent, usersController.deleteUser);
 router.get('/:user_id', userMiddle.isUserPresent, usersController.getSingleUser);
+router.put('/:user_id', userMiddle.isUserPresent, userMiddle.checkUniqueName, usersController.updateUser);
 
 module.exports = router;
