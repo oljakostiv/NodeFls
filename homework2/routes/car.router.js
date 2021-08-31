1125095002537b731551bb7474876277d45d2c6b
oljakostiv <oljakostivv@gmail.com>
2021-08-31 23:06:21 +0300

    some change.

--- homework2/routes/car.router.js
+++ homework2/routes/car.router.js
@@ -2,28 +2,45 @@ const router = require('express').Router();
 
 const { carsController } = require('../controllers');
 const { carMiddle } = require('../middlewares');
+const {
+    constants: {
+        BODY,
+        CAR_ID,
+        ID,
+        QUERY,
+        PARAMS
+    }
+} = require('../config');
+const {
+    carValidator: {
+        createCarValidator,
+        queryCarValidator,
+        paramsCarValidator,
+        updateCarValidator
+    }
+} = require('../validators');
 
 router.get('/',
-    carMiddle.getCarsByDynamicParam('queryCarValidator', 'query'),
+    carMiddle.getCarsByDynamicParam(queryCarValidator, QUERY),
     carsController.getAllCars);
 
 router.post('/',
-    carMiddle.getCarsByDynamicParam('createCarValidator', 'body'),
+    carMiddle.getCarsByDynamicParam(createCarValidator, BODY),
     carMiddle.checkUniqueModel,
     carsController.setCar);
 
 router.delete('/:car_id',
-    carMiddle.getCarsByDynamicParam('paramsCarValidator'),
+    carMiddle.getCarsByDynamicParam(paramsCarValidator),
     carsController.deleteCar);
 
 router.get('/:car_id',
-    carMiddle.getCarsByDynamicParam('paramsCarValidator'),
-    carMiddle.getCarByDynamicParam('car_id', 'params', '_id'),
+    carMiddle.getCarsByDynamicParam(paramsCarValidator),
+    carMiddle.getCarByDynamicParam(CAR_ID, PARAMS, ID),
     carsController.getSingleCar);
 
 router.put('/:car_id',
-    carMiddle.getCarsByDynamicParam('paramsCarValidator'),
-    carMiddle.getCarsByDynamicParam('updateCarValidator'),
+    carMiddle.getCarsByDynamicParam(paramsCarValidator),
+    carMiddle.getCarsByDynamicParam(updateCarValidator),
     carMiddle.checkUniqueModel,
     carsController.updateCar);
 
--- homework2/routes/users.router.js
+++ homework2/routes/users.router.js
@@ -1,5 +1,4 @@
-const router = require('express')
-    .Router();
+const router = require('express').Router();
 
 const { usersController } = require('../controllers');
 const { userMiddle } = require('../middlewares');
