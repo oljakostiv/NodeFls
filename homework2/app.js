882e811270d54789666515955be28bf73ca2cd06
oljakostiv <oljakostivv@gmail.com>
2021-09-05 23:09:33 +0300

    some change.

--- homework2/app.js
+++ homework2/app.js
@@ -3,87 +3,118 @@ const expressHbs = require('express-handlebars');
 const fs = require('fs');
 const path = require('path');
 
-const {PORT} = require('./config/variables');
-const users = require('./db/users.json');
+const { PORT } = require('./config/variables');
+const usersDb = require('./db/users.js');
 
-const user = path.join(__dirname, 'db', 'users.json');
+const users = JSON.parse(JSON.stringify(usersDb));
+const userSet = path.join(__dirname, 'db', 'users.js');
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, 'static')));
 app.set('view engine', '.hbs');
-app.engine('.hbs', expressHbs({defaultLayout: false}));
+app.engine('.hbs', expressHbs({ defaultLayout: false }));
 app.set('views', path.join(__dirname, 'static'));
 
-// app.get('/ping', ((req, res) => {
-//     res.json('pong');
-// }));
+app.get('/ping', ((req, res) => {
+    res.json('pong');
+}));
 
 app.get('/', (req, res) => {
     console.log(req);
-    res.send('<h1>Hello!</h1>');
+    res.render('home');
 });
 
-app.get('/users', ((req, res) => {
-    res.render('users', {users});
-}));
+app.get('/auth', (req, res) => {
+    res.render('login');
+});
 
-app.get('/users/:user_id', ((req, res) => {
-    const {user_id} = req.params;
+app.post('/auth', (req, res) => {
+    const { name } = req.body;
+
+    usersDb.forEach(((value, index) => {
+
+        if (value.name === name) {
+            res.redirect('/users/' + index)
+            return;
+        }
+
+        res.status(404).render('reg', { about: 'Register, please!' });
+    }))
+
+    // const loginUser = users.find(value => value.name = name);
+    //
+    // if (!loginUser) {
+    //     res.status(404)
+    //         .render('reg', { about: 'Register, please!' });
+    // }
+    //
+    // res.redirect('/users/');
+    //
+    // return;
+});
+
+app.get('/registration', (req, res) => {
+    res.render('reg');
+});
+
+app.post('/registration', (req, res) => {
+
+    const {
+        name,
+        age,
+        gender,
+        email,
+        password
+    } = req.body;
+
+    for (let user of usersDb) {
+        if (!name || !age || !gender || !email || !password) {
+            res.status(404)
+                .json('Fill in each item!');
+            return;
+        }
+    }
+
+    usersDb.push({
+        name,
+        age,
+        gender,
+        email,
+        password
+    });
+
+    const newUser = `module.exports = ${JSON.stringify(usersDb)}`;
+
+    fs.writeFile(userSet, newUser, (err) => {
+        if (err) console.log(err);
+    });
+
+    res.status(201)
+        .render('login');
+});
+
+app.get('/users', (req, res) => {
+    res.render('users', { users });
+});
+
+app.get('/users/:user_id', (req, res) => {
+    const { user_id } = req.params;
+
     const currentUser = users[user_id];
 
     if (!currentUser) {
-        res.status(404).end('User not found.');
+        res.status(404)
+            .end('User not found.');
+
         return;
-    };
-    res.json(currentUser);
-}));
+    }
 
-app.get('/auth', ((req, res) => {
-    res.render('login');
-}));
-
-app.post('/auth', ((req, res) => {
-    const {name} = req.body;
-    // const loginUser = fs.readFileSync(path.resolve(__dirname, 'users.json')).toString('utf8')
-    // if (loginUser) {
-    //     res.status(404).render('reg', {about: 'Register, please!'});
-    //     return;
-    // };
-    res.redirect(`/users`);
-}));
-
-app.get('/registration', ((req, res) => {
-    res.render('reg');
-}));
-
-app.post('/registration', ((req, res) => {
-    const {name, age, gender, email, password} = req.body;
-
-    fs.writeFile(user, `${JSON.stringify(users)}`, err => {
-        console.log(err);
-    });
-
-    // fs.readFile(users, ((err, data) => {
-    //     if (err) {
-    //         res.status(404).end('Need to change the name');
-    //         return;
-    //     };
-    //     fs.appendFile(user, data, err1 => {
-    //         console.log(err1);
-    //     });
-    //     res.json(data.toString(users));
-    // }));
-
-    if (!name || !age || !gender || !email || !password) {
-        res.status(404).end('Fill in each item!');
-        return;
-    };
-    res.render('login');
-}));
+    res.render('user', { currentUser });
+});
 
 app.listen(PORT, () => {
     console.log('Hello', PORT);
---
+++ homework2/db/users.js
@@ -0,0 +1,27 @@
+module.exports = [{
+    'name': 'Dima',
+    'age': 25,
+    'gender': 'male',
+    'email': 'dima@gmail.com'
+}, {
+    'name': 'Vira',
+    'age': 21,
+    'gender': 'female',
+    'email': 'vira@gmail.com'
+}, {
+    'name': 'Petro',
+    'age': 27,
+    'gender': 'male',
+    'email': 'petro@gmail.com'
+}, {
+    'name': 'Olena',
+    'age': 24,
+    'gender': 'female',
+    'email': 'olena@gmail.com'
+}, {
+    'name': 'Olha',
+    'age': '25',
+    'gender': 'female',
+    'email': 'oljakostivv@gmail.com',
+    'password': '12345'
+}];
